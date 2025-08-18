07562d15914b9df5e7578488dcb2e3bc13a92a4f
Arina <matyukova.arina27@gmail.com>
2025-08-18 22:15:33 +1000

    настроена автоматическая проверка качества кода(eslint, SonarQube)

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync'
 export default () => {
-  const name = readlineSync.question("May I have your name? ");
-  let greeting = "Hello, " + name + "!";
-  console.log(greeting);
-};
+  const name = readlineSync.question('May I have your name?')
+  let greeting = 'Hello, ' + name + '!'
+  console.log(greeting)
+}
