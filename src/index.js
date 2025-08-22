f9f3c42f3d2050a98ff202432c54f2a32f2ef682
Arina <matyukova.arina27@gmail.com>
2025-08-22 22:20:59 +1000

    выделена общая для игр логика

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import readlineSync from "readline-sync";
+
+export const getName = () => {
+  const name = readlineSync.question("May I have your name? ");
+  return name;
+};
+export const getRandomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+export const greeting = (name, condition) => {
+  console.log(`Hello, ${name}!`);
+  console.log(condition);
+};
+export const getUserAnswer = () => {
+  const answer = readlineSync.question("Your answer: ");
+  return answer;
+};
+export const check = (name, answer, rightAnswer) => {
+  if (rightAnswer === answer) {
+    console.log("Correct!");
+    return true;
+  } else {
+    console.log(
+      `'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`
+    );
+    console.log(`Let's try again, ${name}!`);
+    return false;
+  }
+};
+export const congratulations = (name) => {
+  console.log(`Congratulations, ${name}!`);
+};
